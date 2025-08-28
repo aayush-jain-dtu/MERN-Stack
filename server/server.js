ac60769867990b6f74f9f8ee1a8b2d70c81a45e5
Aayush Jain <167526387+aayush-jain-dtu@users.noreply.github.com>
2025-08-29 02:13:00 +0530

    Update server.js
    adding vercel link

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ connectDB();
 
 app.use(cors({ origin: ["http://localhost:5173",
                         "http://localhost:3000",        
-                      "https://your-app.vercel.app"   
+                      "listiphy-mern.vercel.app"   
                         ] }));
 app.use(express.json());
 
